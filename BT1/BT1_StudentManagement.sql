5b4c4efc801611b6d4e60852f0a643eb94e14ba5
LeDucPhat-C0223H1 <leducphat2808@gmail.com>
2023-10-31 14:37:59 +0700

    nop bai tap 1 31/10/2023

--- BT1/BT1_StudentManagement.sql
+++ BT1/BT1_StudentManagement.sql
@@ -1,1 +1,15 @@
-SELECT * FROM `student-management`.teacher;
+-- Tạo database
+CREATE SCHEMA `student-management` ;
+
+-- Xóa database
+-- DROP DATABASE `student-management`;
+
+CREATE TABLE `student-management`.`class`(
+  `id` INT primary key auto_increment,
+  `name` VARCHAR(45));
+  
+  CREATE TABLE `student-management`.`teacher`(
+  `id` INT primary key auto_increment,
+  `name` VARCHAR(45),
+  `age` INT,
+  `country` VARCHAR(45));
